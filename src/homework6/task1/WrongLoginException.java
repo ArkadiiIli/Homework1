e1cf95386ce48a9fd184206ba24339cb509a1557
Arkadii Ilinov <arkadii@ark-software.com>
2024-05-31 18:01:00 +0200

    Homework6

---
+++ src/homework6/task1/WrongLoginException.java
@@ -0,0 +1,12 @@
+package homework6.task1;
+
+public class WrongLoginException extends Exception {
+
+    public WrongLoginException() {
+        super();
+    }
+
+    public WrongLoginException(String message) {
+        super(message);
+    }
+}

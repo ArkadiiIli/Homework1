e599d461b1c41fef8cfbad9e5d832ea538271481
Arkadii Ilinov <arkadii@ark-software.com>
2024-06-08 18:11:24 +0200

    Fixed homework8/Runnable

--- src/main/java/com/example/homework8/Runnable.java
+++ src/main/java/com/example/homework8/Runnable.java
@@ -3,6 +3,6 @@ package com.example.homework8;
 public class Runnable {
 
     public static void main(String[] args) {
-        System.out.println("hello World!");
+        System.out.println("Hello World!");
     }
 }

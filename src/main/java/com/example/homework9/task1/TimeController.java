9ae9f14af3b04f3764e87a292fabc49ee353a2c8
Arkadii Ilinov <arkadii@ark-software.com>
2024-06-14 18:32:13 +0200

    homework9

---
+++ src/main/java/com/example/homework9/task1/TimeController.java
@@ -0,0 +1,37 @@
+package com.example.homework9.task1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class TimeController {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final String MINSK_ZONE = "+3";
+    private static final String WASHINGTON_ZONE = "-4";
+    private static final String BEIJING_ZONE = "+8";
+
+    @GetMapping("/minsk")
+    public String getMinskTime() {
+        return getCurrentTime(MINSK_ZONE);
+    }
+
+    @GetMapping("/washington")
+    public String getWashingtonTime() {
+        return getCurrentTime(WASHINGTON_ZONE);
+    }
+
+    @GetMapping("/beijing")
+    public String getBeijingTime() {
+        return getCurrentTime(BEIJING_ZONE);
+    }
+
+    private String getCurrentTime(String timeZone) {
+        ZoneOffset zoneOffset = ZoneOffset.of(timeZone);
+        ZonedDateTime currentTime = ZonedDateTime.now(zoneOffset);
+        return formatter.format(currentTime);
+    }
+}
---
+++ src/main/java/com/example/homework9/task2/AgeController.java
@@ -0,0 +1,18 @@
+package com.example.homework9.task2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AgeController {
+    private final static int ADULT_AGE = 18;
+    private final static String ADULT_TEXT = "Adult";
+    private final static String MINOR_TEXT = "Minor";
+
+    @GetMapping("/")
+    public String getAgeStatus(@RequestParam int age) {
+        if (age >= ADULT_AGE) return ADULT_TEXT;
+        else return MINOR_TEXT;
+    }
+}
